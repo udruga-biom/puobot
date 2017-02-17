5dc3a47fd8005c16a6db4ac60e610c3ecf4cdf9d
mzec <mate.zec@gmail.com>
2017-02-17 16:25:25 +0100

    update

--- puobot.py
+++ puobot.py
@@ -24,9 +24,19 @@ for i in link_elem:
     link.append(url + i['href'])
     print(i.text)
 
+tablica = []
 for url_g in link:
     print('obrađujem: ' + url_g)
     r = requests.get(url_g)
     soup = BeautifulSoup(r, 'lxml')
-
+    sadrzaj = soup.find_all('div', 'accordion')[0]
+    zahvat_ime = sadrzaj.find_all('h3', recursive = False)
+    zahvat_kat = sadrzaj.find_all('div', recursive = False)
+    if len(zahvat_ime) != len(zahvat_kat):
+        print('broj zahvata i kategorija se ne podudara')
+    else:
+        n_zahvata = len(zahvat_ime)
+        for zahvat in range(n_zahvata):
+            find.all       
+            naziv = zahvat_ime[zahvat]
 
