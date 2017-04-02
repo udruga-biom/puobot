3a3ab7943e3dc4c888c1986dc73d0a795f932007
narimiran <miran.tuhtan+git@gmail.com>
2017-04-02 19:09:41 +0200

    dodaje nove docstringove

--- puobot.py
+++ puobot.py
@@ -21,7 +21,11 @@ BASE_URL = 'http://puo.mzoip.hr/hr/'
 
 
 def get_twitter_instance():
-    """Kreira twitter instancu"""
+    """Kreira twitter instancu.
+
+    Returns:
+        twython.Twython instanca.
+    """
     import twython
     with open('input/twit_api_data.txt', 'r') as f:
         twython_api_data = f.read().splitlines()
@@ -30,7 +34,7 @@ def get_twitter_instance():
 
 
 def kreiranje_foldera():
-    """kreiranje output foldera za prvo pokretanje"""
+    """Kreira output foldere kod prvog pokretanja."""
     if 'output' not in os.listdir():
         os.mkdir('output')
     if 'arhiva' not in os.listdir('output'):
@@ -40,7 +44,14 @@ def kreiranje_foldera():
 
 
 def puosave(save_dir, postupci):
-    """funkcija za snimanje"""
+    """Snima sve postupke.
+
+    Snimanje svih postupaka u .tsv formatu, u odabrani `save_dir` direktorij.
+
+    Args:
+        save_dir (string): relativni path do direktorija za snimanje.
+        postupci (list): popis svih postupaka koje se snima.
+    """
     imena = ['puo', 'puo_pg', 'opuo', 'spuo_min', 'spuo_pg', 'spuo_jlrs', 'ospuo']
     for filename, postupak in zip(imena, postupci):
         with open(save_dir + filename + '.tsv', 'w') as f:
@@ -48,37 +59,86 @@ def puosave(save_dir, postupci):
 
 
 def puoread(read_dir, filename):
-    """funkcija za čitanje"""
+    """Citanje postupka.
+
+    Args:
+        read_dir (string): relativni path do direktorija iz kojeg se cita.
+        filename (string): ime datoteke koju se ucitava (bez ekstenzije).
+    Returns:
+        list: sadrzaj datoteke.
+    """
     with open(read_dir + filename + '.tsv', 'r') as f:
         in_file = f.read().splitlines()
     return in_file
 
 
 def get_sadrzaj(url, clan=0):
+    """Dohvaca sadrzaj s mreznih stranica.
+
+    Args:
+        url (string): path do mreznih stranica.
+        clan (int): indeks clana kojeg se trazi.
+    Returns:
+        string: sadrzaj zeljenog clana.
+    """
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'lxml')
     return soup.find_all('div', 'accordion')[clan]
 
 
 def get_zahvat(url):
+    """Trazi ime i kategoriju zahvata.
+
+    Pozivaju je `parse_postupaka` i `parse_alt` funkcije.
+
+    Args:
+        url (string): path do mreznih stranica.
+    Returns:
+        tuple: ime i kategorija zahvata.
+    """
     sadrzaj = get_sadrzaj(url)
     zahvat_ime = sadrzaj.find_all('h3', recursive=False)
     zahvat_kat = sadrzaj.find_all('div', recursive=False)
     return zahvat_ime, zahvat_kat
 
 
-def puoscrape(urlname, postupak='puo'):
-    """funkcija za parse PUO/OPUO"""
+def trazenje_postupaka(postupak):
+    """Trazi PUO/OPUO postupke.
+
+    Obavjestava o trazenju postupka, poziva je `parse_postupaka` funkcija.
+
+    Args:
+        postupak (string): postupak koji se trazi, puo ili opuo.
+    Returns:
+        list: postupci po godinama.
+    """
+    print('tražim {} postupke...'.format(postupak.upper()))
+    url = BASE_URL + '{}.html'.format(postupak)
+
     if postupak == 'puo':
         pattern = re.compile('PUO postupci 2[0-9]{3}')
     elif postupak == 'opuo':
         pattern = re.compile('OPUO postupci 2[0-9]{3}')
 
-    r = requests.get(urlname)
+    r = requests.get(url)
     soup = BeautifulSoup(r.content, 'lxml')
     link_elem = (soup.find_all('div', 'four mobile-four columns')[2]
                  .find_all('a', text=pattern))
+    return link_elem
 
+
+def parse_postupaka(postupak):
+    """Parsanje postupaka.
+
+    Za svaku pojedinu godinu izvlaci godinu, ime i kategoriju postupka, te
+    link. Vraca listu tih vrijednosti.
+
+    Args:
+        postupak (string): postupak koji se trazi, puo ili opuo.
+    Returns:
+        list: detaljni (godina, ime, kategorija, link) popis svih postupaka.
+    """
+    link_elem = trazenje_postupaka(postupak)
     url = 'http://puo.mzoip.hr'
     output = []
     for godina in link_elem:
@@ -102,9 +162,17 @@ def puoscrape(urlname, postupak='puo'):
     return output
 
 
-def puoscrape_alt(urlname):
-    """funkcija za parse SPUO i prekograničnih postupaka"""
-    zahvati = get_zahvat(urlname)
+def parse_alt(url):
+    """Parsa SPUO i prekogranične postupke.
+
+    Pozivaju je `trazenje_prekogranicnih` i `trazenje_spuo` funkcije.
+
+    Args:
+        url (string): path do mreznih stranica.
+    Returns:
+        list: detaljni (ime, link) popis svih postupaka.
+    """
+    zahvati = get_zahvat(BASE_URL + url)
     output = []
     for ime, kategorija in zip(*zahvati):
         linkovi = kategorija.find_all('a')
@@ -114,40 +182,73 @@ def puoscrape_alt(urlname):
     return output
 
 
-def trazenje_postupaka(postupak):
-    print('tražim {} postupke...'.format(postupak.upper()))
-    url = BASE_URL + '{}.html'.format(postupak)
-    return puoscrape(url, postupak)
-
-
 def trazenje_prekogranicnih(url):
+    """Trazi prekogranične postupke.
+
+    Obavjestava o trazenju postupka, poziva `parse_alt` funkciju.
+
+    Args:
+        url (string): path do mreznih stranica.
+    Returns:
+        list: detaljni (ime, link) popis svih postupaka.
+    """
     postupak = url.split('/')[0]
     print('tražim prekogranične {} postupke...'.format(postupak.upper()))
-    return puoscrape_alt(BASE_URL + url)
+    return parse_alt(url)
+
+
+def parse_jlrs(url):
+    """Parsa SPUO postupke za koje je nadlezno drugo tijelo ili JLRS.
+
+    Poziva je `trazenje_spuo` funkcija.
+
+    Args:
+        url (string): path do mreznih stranica.
+    Returns:
+        list: detaljni (ime, nadleznost, link) popis svih postupaka.
+    """
+    r = requests.get(BASE_URL + url)
+    soup = BeautifulSoup(r.content, 'lxml')
+    sadrzaj = (soup.find_all('h2', text=re.compile('Postupci stra.*'))[0]
+               .parent.parent.find_all('ul')[1])
+    postupci = []
+    for i in sadrzaj.find_all('li'):
+        trazenje = re.search('^(.*?)(Nadle.*?)http.*', i.text)
+        zahvat = trazenje.group(1)
+        nadlezan = trazenje.group(2)
+        link = i.find('a')['href']
+        postupci.append('\t'.join([zahvat, nadlezan, link]))
+    return postupci
 
 
 def trazenje_spuo(url, nadleznost):
+    """Trazi SPUO postupke.
+
+    Ovisno o tome cija je nadleznost (MZOIE ili drugi), poziva ili
+    `parse_alt` ili `parse_jlrs` funkciju.
+
+    Args:
+        url (string): path do mreznih stranica.
+        nadleznost (string): MZOIE ili JLRS.
+    Returns:
+        list: popis postupaka.
+    """
     if nadleznost == 'MZOIE':
         print('tražim SPUO postupke za koje je nadležno MZOIE...')
-        return puoscrape_alt(BASE_URL + url)
+        return parse_alt(url)
     else:
         print('tražim SPUO postupke za koje je nadležno drugo središnje tijelo ili JLRS...')
-        r = requests.get(BASE_URL + url)
-        soup = BeautifulSoup(r.content, 'lxml')
-        sadrzaj = (soup.find_all('h2', text=re.compile('Postupci stra.*'))[0]
-                   .parent.parent.find_all('ul')[1])
-
-        postupci = []
-        for i in sadrzaj.find_all('li'):
-            trazenje = re.search('^(.*?)(Nadle.*?)http.*', i.text)
-            zahvat = trazenje.group(1)
-            nadlezan = trazenje.group(2)
-            link = i.find('a')['href']
-            postupci.append('\t'.join([zahvat, nadlezan, link]))
-        return postupci
+        return parse_jlrs(url)
 
 
 def trazenje_ospuo(url):
+    """Trazi OSPUO postupke.
+
+    Args:
+        url (string): path do mreznih stranica.
+    Returns:
+        list: popis postupaka.
+    """
     print('tražim OSPUO postupke...')
     sadrzaj = get_sadrzaj(BASE_URL + url, clan=1)
 
@@ -160,24 +261,25 @@ def trazenje_ospuo(url):
 
 
 def dohvat_postupaka():
-    # PUO postupci
-    puo_tab = trazenje_postupaka('puo')
-    # OPUO postupci
-    opuo_tab = trazenje_postupaka('opuo')
-    # prekogranični PUO postupci
+    """Dohvat svih postupaka.
+
+    Returns:
+        list: popis svih postupaka.
+    """
+    puo_tab = parse_postupaka('puo')
+    opuo_tab = parse_postupaka('opuo')
     puo_pg_tab = trazenje_prekogranicnih('puo/prekogranicni-postupci'
                                          '-procjene-utjecaja-zahvata-na-okolis.html')
-    # SPUO postupci, prekogranični
     spuo_pg_tab = trazenje_prekogranicnih('spuo/prekogranicni-postupci-strateske-procjene.html')
-    # SPUO postupci, nadležan MZOIE
+
     SPUO_BASE_URL = 'spuo/postupci-strateske-procjene-nadlezno-tijelo-je-'
     url_spuo_min = SPUO_BASE_URL + 'ministarstvo-zastite-okolisa-i-energetike.html'
-    spuo_min_tab = trazenje_spuo(url_spuo_min, 'MZOIE')
-    # SPUO postupci, nadležno drugo središnje tijelo ili jedinice JLRS
+    spuo_min_tab = trazenje_spuo(url_spuo_min, nadleznost='MZOIE')
+
     url_spuo_jlrs = (SPUO_BASE_URL + 'drugo-sredisnje-tijelo-drzavne-uprave'
                      '-ili-jedinica-podrucne-regionalne-ili-lokalne-samouprave.html')
-    spuo_jlrs_tab = trazenje_spuo(url_spuo_jlrs, 'JLRS')
-    # OSPUO postupci
+    spuo_jlrs_tab = trazenje_spuo(url_spuo_jlrs, nadleznost='JLRS')
+
     ospuo_tab = trazenje_ospuo('spuo/ocjena-o-potrebi-provedbe-strateske-procjene.html')
 
     postupci = [puo_tab, puo_pg_tab, opuo_tab, spuo_min_tab, spuo_pg_tab, spuo_jlrs_tab, ospuo_tab]
@@ -187,6 +289,14 @@ def dohvat_postupaka():
 
 
 def citanje_arhive():
+    """Cita podatke iz arhive, ukoliko arhiva postoji.
+
+    Ako arhiva ne postoji, ne ucitava postojece podatke.
+
+    Returns:
+        list: popis svih postupaka iz arhive, ukoliko ona postoji.
+              U suprotnom vraca None.
+    """
     arhiva_dir = os.listdir('output/arhiva/')
     arhiva_dir.sort()
 
@@ -208,6 +318,13 @@ def citanje_arhive():
 
 
 def pisanje_arhive(postupci):
+    """Sprema nove podatke u arhivu.
+
+    Radi novu mapu "trenutni datum i vrijeme" u koju sprema dohvacene postupke.
+
+    Returns:
+        string: ime mape u koju su spremljene datoteke.
+    """
     vrijeme = datetime.now()
     stamp = vrijeme.strftime('%Y-%m-%d-%H-%M')
 
@@ -219,7 +336,16 @@ def pisanje_arhive(postupci):
 
 
 def trazi_razlike(staro, novo):
-    """funkcija koja pronalazi razlike između starih i novih verzija dokumenata"""
+    """Pronalazi razlike između starih i novih verzija dokumenata.
+
+    Pronadjene razlike printa.
+    Ukoliko je prilikom pokretanja programa dodan twitter argument,
+    sve uocene novosti objavljuje na twitteru.
+
+    Args:
+        staro (list): podatci iz arhive.
+        novo (list): dohvaceni podatci.
+    """
     razlike = []
     for old, new in zip(staro, novo):
         razlika = set(new) - set(old)
